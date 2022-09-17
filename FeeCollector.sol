1481a98acead5f74dc21578aa61d04dd12c4baad
Egemen Nair <108150376+EgemenNair@users.noreply.github.com>
2022-09-17 16:19:30 +0300

    Update FeeCollector.sol

--- FeeCollector.sol
+++ FeeCollector.sol
@@ -2,25 +2,36 @@
 pragma solidity 0.8.17;
 
 contract FeeCollector {
+    // Defining the state variables for the owner and balance:
     address public owner;
     uint256 public balance;
 
+    // Setting the constructor to initialize the owner as the one deploying the smart contract:
     constructor() {
         owner = msg.sender;
     }
 
+    // Defining the recieve function as payable to be able to transact with the function:
     receive() payable external {
+        // Setting the balance equal to (balance + amount sent): 
         balance += msg.value;
     }
 
+    // Defining a function modifier for authentication :
     modifier Auth(uint amount) {
+        // Using require to only allow the owner to withdraw :
         require(msg.sender == owner, "Only owner can withdraw");
+        
+        // If the amount to be withdrawed is greater then balance , give an error.
         require(amount <= balance, "Insufficent funds");
+        
+        // Execute the rest of the function.
         _;
     }
 
+    // Defining a function to withdraw, defining destAddr as payable to transact.
     function withdraw(uint amount, address payable destAddr) public Auth(amount) {
-        destAddr.transfer(amount);
-        balance -= amount;
+        destAddr.transfer(amount);      // Transferring to destination address.
+        balance -= amount;      //Updating the balance.
     }
 }
